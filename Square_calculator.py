1a2a77edc5938d66453d78f664962753cf0c41ea
aimbot-git <91827838+aimbot-git@users.noreply.github.com>
2021-11-19 16:02:54 +0530

    Add files via upload

---
+++ Square_calculator.py
@@ -0,0 +1,13 @@
+a=input('what is your name? ')
+print('Hi',a ,'what are you doing')
+b=150
+c=50
+print(b/c)
+print(b+c)
+#pro
+z=input('enter a number ')
+x=input('enter another number ')
+x=int(x)
+z=int(z)
+print('the sum of the given numbers is:',z+x)
+print('hello world')
